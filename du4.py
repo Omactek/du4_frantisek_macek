846fc104fba353cc357bda7c82340913298d37eb
František Macek <frantisek.macek1@gmail.com>
2022-02-19 15:18:26 +0100

    argparse arguments

---
+++ du4.py
@@ -0,0 +1,12 @@
+import argparse
+"""
+parser = argparse.ArgumentParser(description='Test argparse')
+parser.add_argument("-f", help = "input file path", required = True)
+parser.add_argument("-l", help = "segment length", required = True)
+parser.add_argument("-o", help = "out file name", required = True)
+args = parser.parse_args()
+
+in_file_path = args.f
+segment_length = args.l
+out_file_name = args.o
+"""
